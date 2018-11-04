c6fa2d88b824d5703ec773c0ccc39a55b823c904
KSpalinska <kasia.spalinska@gmail.com>
2018-11-04 21:03:55 +0100

    usage of Level class; importing levels from csv

--- Level.py
+++ Level.py
@@ -1,7 +1,7 @@
 import pygame
 from Constants import LEVEL_SURFACE_PATH
 
-class LevelView:
+class Level:
 
     def __init__(self):
         surface = pygame.image.load(LEVEL_SURFACE_PATH).convert()
